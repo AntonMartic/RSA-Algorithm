ad8314f6cb3f66de21eb77969a123ca7b3b3e763
AntonMartic <anton.martic@gmail.com>
2025-09-13 23:09:23 +0200

    More robust code

--- src/rsa.java
+++ src/rsa.java
@@ -1,27 +1,40 @@
 
 import java.math.BigInteger;
-import java.util.Random;
+import java.security.SecureRandom;
 
 public class rsa {
 
 	private BigInteger privateKey;
+	
 	private BigInteger publicKey;
 	
-	private int length = 64;
+	private BigInteger modulus;
 	
-	private Random random = new Random();
+	// choose 1024 later, 
+	// TODO: factor the modulus n with a simple online tool) to break the encryption with lower bit
+	private int bitLength = 64; 
+	
+	private SecureRandom random;
+	
+	
+	public rsa() {
+		random = new SecureRandom();
+		generateKeys();
+	}
 	
 	public void generateKeys() {
 	
-		BigInteger p = BigInteger.probablePrime(length, random);
-		BigInteger q = BigInteger.probablePrime(length, random);
+		BigInteger p = BigInteger.probablePrime(bitLength, random);
+		BigInteger q = BigInteger.probablePrime(bitLength, random);
 		
 	    while (q.equals(p)) {
-	        q = BigInteger.probablePrime(length, random);
+	        q = BigInteger.probablePrime(bitLength, random);
 	    }
 		
-		BigInteger n = p.multiply(q);
+	    // n = p*q
+		modulus = p.multiply(q);
 		
+		// φ(n) = (p-1) * (q-1)
 		BigInteger phiN = (p.subtract(BigInteger.ONE)).multiply(q.subtract(BigInteger.ONE));
 		
 		// Setting e to biggest Fermat number 65537
